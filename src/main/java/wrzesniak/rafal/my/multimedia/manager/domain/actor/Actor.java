14e8d827490e2fd42b02a22dce80feb4dd571b48
Rafał <rafal.wrzesniak94@gmail.com>
2022-12-09 16:58:19 +0100

    Adds Movie and Actor domain classes

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/actor/Actor.java
@@ -0,0 +1,87 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.actor;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+import wrzesniak.rafal.my.multimedia.manager.domain.movie.Movie;
+import wrzesniak.rafal.my.multimedia.manager.web.filmweb.FilmwebSearchable;
+
+import javax.persistence.*;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@With
+@Builder
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Actor implements FilmwebSearchable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String imdbId;
+
+    private String name;
+    private LocalDate birthDate;
+    private LocalDate deathDate;
+
+    private URL filmwebUrl;
+
+    @ManyToMany
+    @JsonBackReference
+    @ToString.Exclude
+    private List<Movie> playedInMovies;
+    @ManyToMany
+    @JsonBackReference
+    @ToString.Exclude
+    private List<Movie> directedMovies;
+    @ManyToMany
+    @JsonBackReference
+    @ToString.Exclude
+    private List<Movie> wroteMovies;
+
+    public String getImagePath() {
+        return String.format("images/actor/%s.jpg", imdbId);
+    }
+
+    @Override
+    public String getFilmwebSearchString() {
+        return name;
+    }
+
+    public Integer getAge() {
+        if(birthDate == null) return null;
+        return Period.between(
+                birthDate,
+                Optional.ofNullable(deathDate).orElse(LocalDate.now())
+        ).getYears();
+    }
+
+    public void addToMovieAs(Movie movie, Role role) {
+        if(Role.Actor.equals(role)) {
+            addToListIfDoNotExist(playedInMovies, movie);
+        } else if (Role.Director.equals(role)) {
+            addToListIfDoNotExist(directedMovies, movie);
+        } else if(Role.Writer.equals(role)) {
+            addToListIfDoNotExist(wroteMovies, movie);
+        }
+    }
+
+    private void addToListIfDoNotExist(List<Movie> list, Movie movie) {
+        if(list == null) {
+            list = new ArrayList<>();
+        }
+        if(!list.contains(movie)) {
+            list.add(movie);
+        }
+    }
+
+}
---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/Movie.java
@@ -0,0 +1,97 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.movie;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
+import wrzesniak.rafal.my.multimedia.manager.domain.actor.Actor;
+import wrzesniak.rafal.my.multimedia.manager.domain.actor.Role;
+import wrzesniak.rafal.my.multimedia.manager.web.filmweb.FilmwebSearchable;
+
+import javax.persistence.*;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@With
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie implements FilmwebSearchable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String imdbId;
+
+    private String title;
+    private String polishTitle;
+    private LocalDate releaseDate;
+    private Integer runtimeMins;
+    private double imDbRating;
+    private Integer imDbRatingVotes;
+
+    private URL filmwebUrl;
+
+    @ToString.Exclude
+    @JsonManagedReference
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Actor> actorList;
+    @ToString.Exclude
+    @JsonManagedReference
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Actor> directorList;
+    @ToString.Exclude
+    @JsonManagedReference
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Actor> writerList;
+
+    @ElementCollection
+    private List<String> genreList = new ArrayList<>();
+    @ElementCollection
+    private List<String> countryList = new ArrayList<>();
+
+    @ToString.Exclude
+    private String plotLocal;
+
+    public String getImagePath() {
+        return String.format("images/movie/%s.jpg", imdbId);
+    }
+
+    @Override
+    public String getFilmwebSearchString() {
+        return String.format("%s (%s)", title, releaseDate.getYear());
+    }
+
+    public void addRole(Actor actor, Role role) {
+        if(Role.Actor.equals(role)) {
+            addToListIfDoNotExist(actorList, actor);
+        } else if (Role.Director.equals(role)) {
+            addToListIfDoNotExist(directorList, actor);
+        } else if(Role.Writer.equals(role)) {
+            addToListIfDoNotExist(writerList, actor);
+        } else {
+            return;
+        }
+        actor.addToMovieAs(this, role);
+    }
+
+    public void addActorsWithRole(List<Actor> actorsToAdd, Role role) {
+        actorsToAdd.forEach(actor -> addRole(actor, role));
+    }
+
+    private void addToListIfDoNotExist(List<Actor> list, Actor actor) {
+        if(list == null) {
+            list = new ArrayList<>();
+        }
+        if(!list.contains(actor)) {
+            list.add(actor);
+        }
+    }
+
+}
