93456f15176fdf015c4aa6bd7a44a0f891cf4062
Rafał <rafal.wrzesniak94@gmail.com>
2022-12-11 15:36:27 +0100

    Minor refactor

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/web/WebOperations.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/web/WebOperations.java
@@ -1,7 +1,11 @@
 package wrzesniak.rafal.my.multimedia.manager.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
+import wrzesniak.rafal.my.multimedia.manager.web.imdb.ImdbService;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,16 +16,26 @@ import java.nio.file.StandardCopyOption;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebOperations {
 
-    public boolean downloadImageToDirectory(URL imageUrl, Path pathToSaveImage) {
+    private final ImdbService imdbService;
+
+    public void downloadResizedImageTo(URL originalImageUrl, Path savePath) {
+        URL resizedImageUrl = imdbService.findResizedImageUrl(originalImageUrl);
+        downloadImageToDirectory(resizedImageUrl, savePath);
+    }
+
+    private void downloadImageToDirectory(URL imageUrl, Path pathToSaveImage) {
         try (InputStream inputStream = imageUrl.openStream()) {
             Files.copy(inputStream, pathToSaveImage, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             log.warn("Failed to download image from \"{}\"", imageUrl);
-            return false;
         }
         log.debug("Image downloaded from \"{}\"", imageUrl);
-        return true;
+    }
+
+    public Document parseUrl(URL websiteUrl) throws IOException {
+        return Jsoup.parse(websiteUrl, 3000);
     }
 }
--- src/main/java/wrzesniak/rafal/my/multimedia/manager/web/filmweb/FilmwebSearchable.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/web/filmweb/FilmwebSearchable.java
@@ -1,7 +1,10 @@
 package wrzesniak.rafal.my.multimedia.manager.web.filmweb;
 
+import java.net.URL;
+
 public interface FilmwebSearchable {
 
     String getFilmwebSearchString();
+    void setFilmwebUrl(URL filmwebUrl);
 
 }
