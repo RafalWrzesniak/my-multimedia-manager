ec46dc835bedd6c6d91beff8af19729181d4f07d
Rafał <rafal.wrzesniak94@gmail.com>
2022-12-16 17:59:20 +0100

    Adds finding movie by filmweb url in repository

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/MovieRepository.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/MovieRepository.java
@@ -3,6 +3,7 @@ package wrzesniak.rafal.my.multimedia.manager.domain.movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.net.URL;
 import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
@@ -10,4 +11,6 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     @Query("SELECT m FROM Movie m WHERE m.imdbId = ?1")
     Optional<Movie> findByImdbId(String imdbId);
 
+    @Query("SELECT m FROM Movie m WHERE m.filmwebUrl = ?1")
+    Optional<Movie> findByFilmwebUrl(URL filmwebMovieUrl);
 }
