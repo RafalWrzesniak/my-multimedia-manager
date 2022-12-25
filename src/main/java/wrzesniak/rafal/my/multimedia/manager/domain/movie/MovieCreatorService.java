13ccd0e45b5f29328c1a3591fcdf9752d3b7d637
Rafał <rafal.wrzesniak94@gmail.com>
2022-12-25 13:39:07 +0100

    Adds movie creator service

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/MovieCreatorService.java
@@ -0,0 +1,121 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.movie;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wrzesniak.rafal.my.multimedia.manager.aop.TrackExecutionTime;
+import wrzesniak.rafal.my.multimedia.manager.domain.actor.Actor;
+import wrzesniak.rafal.my.multimedia.manager.domain.actor.ActorManagementService;
+import wrzesniak.rafal.my.multimedia.manager.domain.actor.Role;
+import wrzesniak.rafal.my.multimedia.manager.domain.mapper.ActorInMovieDto;
+import wrzesniak.rafal.my.multimedia.manager.domain.mapper.DtoMapper;
+import wrzesniak.rafal.my.multimedia.manager.util.Validators;
+import wrzesniak.rafal.my.multimedia.manager.web.WebOperations;
+import wrzesniak.rafal.my.multimedia.manager.web.filmweb.FilmwebService;
+import wrzesniak.rafal.my.multimedia.manager.web.imdb.ImdbService;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+
+import static wrzesniak.rafal.my.multimedia.manager.util.StringFunctions.fileNameOf;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MovieCreatorService {
+
+    private static final int ACTORS_TO_DOWNLOAD = 10;
+    private static final int CREW_TO_DOWNLOAD = 4;
+
+    private final Validators validators;
+    private final ImdbService imdbService;
+    private final WebOperations webOperations;
+    private final FilmwebService filmwebService;
+    private final MovieRepository movieRepository;
+    private final ActorManagementService actorManagementService;
+
+    @Transactional
+    public Optional<Movie> createMovieFromFilmwebUrl(URL filmwebMovieUrl) {
+        if(!validators.isValidFilmwebMovieUrl(filmwebMovieUrl)) {
+            log.warn("Provided movie url is not a valid one: {}", filmwebMovieUrl);
+            return Optional.empty();
+        }
+        Optional<Movie> movieInDatabase = movieRepository.findByFilmwebUrl(filmwebMovieUrl);
+        if(movieInDatabase.isPresent()) {
+            log.info("Movie with url `{}` already exists in database: {}", filmwebMovieUrl, movieInDatabase);
+            return movieInDatabase;
+        }
+        String polishTitle = filmwebService.findTitleFromUrl(filmwebMovieUrl);
+        if(polishTitle == null) {
+            log.warn("Cannot find movie title basing on provided url: {}", filmwebMovieUrl);
+            return Optional.empty();
+        }
+        return createMovieFromPolishTitle(polishTitle);
+    }
+
+    @Transactional
+    public Optional<Movie> createMovieFromLocalFolder(File file) {
+        return createMovieFromPolishTitle(fileNameOf(file));
+    }
+
+    @Transactional
+    @TrackExecutionTime
+    public Optional<Movie> createMovieFromPolishTitle(String polishTitle) {
+        Optional<MovieDto> optionalBestMovieForSearchByTitle = imdbService.findBestMovieForSearchByTitle(polishTitle);
+        if(optionalBestMovieForSearchByTitle.isEmpty()) {
+            return Optional.empty();
+        }
+        MovieDto movieDto = optionalBestMovieForSearchByTitle.orElseThrow();
+        Optional<Movie> movieInDataBase = movieRepository.findByImdbId(movieDto.getId());
+        if(movieInDataBase.isPresent()) {
+            log.info("Movie with title `{}` already exists in database: {}", polishTitle, movieInDataBase.get());
+            return movieInDataBase;
+        }
+        Movie movie = DtoMapper.mapToMovie(movieDto);
+        filmwebService.addFilmwebUrlTo(movie);
+        webOperations.downloadResizedImageTo(movieDto.getImage(), movie.getImagePath());
+        formatPlotLocal(movie);
+        addFullCastToMovie(movie, movieDto);
+        Movie savedMovie = movieRepository.save(movie);
+        log.info("Movie saved in database: {}", savedMovie);
+        return Optional.of(savedMovie);
+    }
+
+    private void formatPlotLocal(Movie movie) {
+        String plotLocal = movie.getPlotLocal();
+        if(plotLocal.matches(".+? \\[.+?\\]$")) {
+            String formattedPlotLocal = plotLocal.substring(0, plotLocal.lastIndexOf(" ["));
+            movie.setPlotLocal(formattedPlotLocal);
+        }
+    }
+
+    private void addFullCastToMovie(Movie movie, MovieDto movieDto) {
+        log.info("Adding cast for movie {}", movie);
+        addActorsToMovie(movie, movieDto.getActorList(), Role.Actor);
+        log.info("Adding directors for movie {}", movie);
+        addActorsToMovie(movie, movieDto.getDirectorList(), Role.Director);
+        log.info("Adding writers for movie {}", movie);
+        addActorsToMovie(movie, movieDto.getWriterList(), Role.Writer);
+    }
+
+    private void addActorsToMovie(Movie movie, List<ActorInMovieDto> actorsInMovieDtos, Role actorsRole) {
+        int actorsNumberToDownload = Role.Actor.equals(actorsRole) ? ACTORS_TO_DOWNLOAD : CREW_TO_DOWNLOAD;
+        log.info("Trying to download and add with Role `{}` actors: {}", actorsRole, actorsInMovieDtos);
+        List<Actor> actors = createActorsFromActorsInMovieDto(actorsInMovieDtos, actorsNumberToDownload);
+        movie.addActorsWithRole(actors, actorsRole);
+    }
+
+    private List<Actor> createActorsFromActorsInMovieDto(List<ActorInMovieDto> actorInMovieDtos, long actorsNumber) {
+        return actorInMovieDtos.stream()
+                .map(ActorInMovieDto::id)
+                .map(actorManagementService::createActorFromImdbId)
+                .filter(Optional::isPresent)
+                .limit(actorsNumber)
+                .map(Optional::get)
+                .toList();
+    }
+
+}
---
+++ src/test/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/MovieCreatorServiceTest.java
@@ -0,0 +1,122 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.movie;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import wrzesniak.rafal.my.multimedia.manager.domain.actor.ActorManagementService;
+import wrzesniak.rafal.my.multimedia.manager.util.Validators;
+import wrzesniak.rafal.my.multimedia.manager.web.WebOperations;
+import wrzesniak.rafal.my.multimedia.manager.web.filmweb.FilmwebService;
+import wrzesniak.rafal.my.multimedia.manager.web.imdb.ImdbService;
+
+import java.io.File;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@Profile("test")
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class MovieCreatorServiceTest {
+
+    @Autowired
+    private Validators validators;
+    @Autowired
+    private ImdbService imdbService;
+    @Autowired
+    private WebOperations webOperations;
+    @Autowired
+    private FilmwebService filmwebService;
+    @MockBean
+    private ActorManagementService actorManagementService;
+    @MockBean
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private MovieCreatorService movieCreatorService;
+
+    private static final String MATRIX_TITLE = "Matrix";
+
+
+    @SneakyThrows
+    @Test
+    void shouldFindAndCreateMatrixMovie() {
+        // given
+        ArgumentCaptor<Movie> movieParam = ArgumentCaptor.forClass(Movie.class);
+        when(movieRepository.save(movieParam.capture())).thenAnswer((invocation) -> movieParam.getValue());
+        given(actorManagementService.createActorFromImdbId(any())).willReturn(Optional.empty());
+
+        // when
+        Optional<Movie> optionalMovieFromPolishTitle = movieCreatorService.createMovieFromPolishTitle(MATRIX_TITLE);
+        Movie createdMovie = optionalMovieFromPolishTitle.orElseThrow();
+        createdMovie.setImDbRating(8.7);
+        createdMovie.setImDbRatingVotes(1000);
+
+        // then
+        assertEquals("The Matrix", createdMovie.getTitle());
+        assertEquals(MATRIX_TITLE, createdMovie.getPolishTitle());
+        assertEquals(new URL("https://www.filmweb.pl/film/Matrix-1999-628"), createdMovie.getFilmwebUrl());
+        assertEquals(LocalDate.of(1999, 3, 31), createdMovie.getReleaseDate());
+        assertEquals("tt0133093", createdMovie.getImdbId());
+        assertEquals(136, createdMovie.getRuntimeMins());
+        assertTrue(createdMovie.getGenreList().containsAll(List.of("Akcja", "Science fiction")));
+        assertTrue(createdMovie.getCountryList().containsAll(List.of("USA", "Australia")));
+    }
+
+    @SneakyThrows
+    @Test
+    void shouldReturnEmptyWhenWrongFilmwebUrl() {
+
+        // when
+        Optional<Movie> movieFromFilmwebUrl = movieCreatorService.createMovieFromFilmwebUrl(new URL("https://www.bad-url.com"));
+
+        // then
+        assertTrue(movieFromFilmwebUrl.isEmpty());
+    }
+
+    @SneakyThrows
+    @Test
+    void shouldCallCreateMovieFromPolishTitleWhenCreatingFromUrl() {
+        // given
+        ArgumentCaptor<Movie> movieParam = ArgumentCaptor.forClass(Movie.class);
+        when(movieRepository.save(movieParam.capture())).thenAnswer((invocation) -> movieParam.getValue());
+        given(actorManagementService.createActorFromImdbId(any())).willReturn(Optional.empty());
+        MovieCreatorService creatorServiceSpy = Mockito.spy(new MovieCreatorService(validators, imdbService, webOperations, filmwebService, movieRepository, actorManagementService));
+
+        // when
+        creatorServiceSpy.createMovieFromFilmwebUrl(new URL("https://www.filmweb.pl/film/Matrix-1999-628"));
+
+        // then
+        verify(creatorServiceSpy).createMovieFromPolishTitle("Matrix (1999)");
+    }
+
+    @Test
+    void shouldCallCreateMovieFromPolishTitleWhenCreatingPath() {
+        // given
+        ArgumentCaptor<Movie> movieParam = ArgumentCaptor.forClass(Movie.class);
+        when(movieRepository.save(movieParam.capture())).thenAnswer((invocation) -> movieParam.getValue());
+        given(actorManagementService.createActorFromImdbId(any())).willReturn(Optional.empty());
+        MovieCreatorService creatorServiceSpy = Mockito.spy(new MovieCreatorService(validators, imdbService, webOperations, filmwebService, movieRepository, actorManagementService));
+
+        // when
+        creatorServiceSpy.createMovieFromLocalFolder(new File("E:\\Rafał\\Filmy\\Matrix\\Matrix.mkv"));
+
+        // then
+        verify(creatorServiceSpy).createMovieFromPolishTitle("Matrix");
+    }
+}
