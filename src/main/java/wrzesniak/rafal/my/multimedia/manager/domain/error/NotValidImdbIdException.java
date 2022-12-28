54677db3567d97a96311044e51c3f8c4c993b997
Rafał <rafal.wrzesniak94@gmail.com>
2022-12-28 21:37:19 +0100

    Defines basic errors

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/error/NotValidImdbIdException.java
@@ -0,0 +1,4 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.error;
+
+public class NotValidImdbIdException extends RuntimeException {
+}
---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/error/UserAlreadyExistException.java
@@ -0,0 +1,9 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.error;
+
+public class UserAlreadyExistException extends Exception {
+
+    public UserAlreadyExistException(String s) {
+        super(s);
+    }
+
+}
