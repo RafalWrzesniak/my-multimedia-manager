d0ec0dbd505db3b41cf364ef5902df3fd426f1a9
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-02 22:45:48 +0100

    Adds user controller

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/UserController.java
@@ -0,0 +1,24 @@
+package wrzesniak.rafal.my.multimedia.manager.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.User;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.UserRepository;
+
+@RestController
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/")
+    public User getCurrentUser() {
+        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+}
