778ba33d67eaa7d1420511af6b1e824d5c0baf6d
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-02 22:46:09 +0100

    Adds login controller

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/LoginController.java
@@ -0,0 +1,15 @@
+package wrzesniak.rafal.my.multimedia.manager.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import wrzesniak.rafal.my.multimedia.manager.config.security.LoginCredentials;
+
+@RestController
+public class LoginController {
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginCredentials credentials) {
+    }
+
+}
