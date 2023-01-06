7f0babbfef6d2c5990f2d1a5966786a34033c89a
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-06 12:48:39 +0100

    Adds validation for credentials and login controller

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/config/security/LoginCredentials.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/config/security/LoginCredentials.java
@@ -3,11 +3,20 @@ package wrzesniak.rafal.my.multimedia.manager.config.security;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @AllArgsConstructor
 public class LoginCredentials {
 
+    public static final String USERNAME_VALIDATION_REGEX = "^[a-zA-Z][\\w.@_]{3,50}$";
+    public static final String USERNAME_VALIDATION_MESSAGE = "Username must be between 4-50 letters, start with letter and contain only letters, digits, dots, at signs and underscore";
+    public static final String PASSWORD_VALIDATION_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+    public static final String PASSWORD_VALIDATION_MESSAGE = "Password must has minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
+
+    @Pattern(regexp = USERNAME_VALIDATION_REGEX, message = USERNAME_VALIDATION_MESSAGE)
     private String username;
+    @Pattern(regexp = PASSWORD_VALIDATION_REGEX, message = PASSWORD_VALIDATION_MESSAGE)
     private String password;
 
 }
--- src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/LoginController.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/LoginController.java
@@ -1,15 +1,18 @@
 package wrzesniak.rafal.my.multimedia.manager.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import wrzesniak.rafal.my.multimedia.manager.config.security.LoginCredentials;
 
+import javax.validation.Valid;
+
+@Validated
 @RestController
 public class LoginController {
 
     @PostMapping("/login")
-    public void login(@RequestBody LoginCredentials credentials) {
+    public void login(@Valid LoginCredentials credentials) {
     }
 
 }
