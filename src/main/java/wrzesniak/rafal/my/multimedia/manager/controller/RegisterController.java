ca084cbdd2d94966e091d9b715d01ec4c540dd66
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-06 12:48:09 +0100

    Adds registration controller

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/RegisterController.java
@@ -0,0 +1,30 @@
+package wrzesniak.rafal.my.multimedia.manager.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wrzesniak.rafal.my.multimedia.manager.config.security.LoginCredentials;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.RegistrationService;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("register")
+@RequiredArgsConstructor
+public class RegisterController {
+
+    private final RegistrationService registrationService;
+
+    @PostMapping("/")
+    public UserDetails registerNewUser(@Valid LoginCredentials credentials) {
+        log.info("Trying to register new user: {}", credentials);
+        return registrationService.registerNewUserAccount(credentials);
+    }
+
+}
