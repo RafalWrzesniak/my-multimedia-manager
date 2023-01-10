0dbeadb083e24db8682e0f44eb20ad0dbb2c63be
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-10 21:24:11 +0100

    Changes user service to two separate services

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/MovieContentListService.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/movie/MovieContentListService.java
@@ -1,34 +1,28 @@
-package wrzesniak.rafal.my.multimedia.manager.domain.user;
+package wrzesniak.rafal.my.multimedia.manager.domain.movie;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import wrzesniak.rafal.my.multimedia.manager.domain.actor.Actor;
-import wrzesniak.rafal.my.multimedia.manager.domain.actor.ActorRepository;
-import wrzesniak.rafal.my.multimedia.manager.domain.content.ActorContentList;
-import wrzesniak.rafal.my.multimedia.manager.domain.content.ContentList;
 import wrzesniak.rafal.my.multimedia.manager.domain.content.MovieContentList;
 import wrzesniak.rafal.my.multimedia.manager.domain.error.MovieNotFoundException;
 import wrzesniak.rafal.my.multimedia.manager.domain.error.NoListWithSuchNameException;
-import wrzesniak.rafal.my.multimedia.manager.domain.movie.Movie;
-import wrzesniak.rafal.my.multimedia.manager.domain.movie.MovieRepository;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.User;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.UserRepository;
 import wrzesniak.rafal.my.multimedia.manager.domain.validation.imdb.ImdbId;
 
 import javax.validation.Valid;
-import java.util.function.Function;
 
 @Slf4j
 @Service
 @Validated
 @Transactional
 @RequiredArgsConstructor
-public class UserService {
+public class MovieContentListService {
 
     private final UserRepository userRepository;
     private final MovieRepository movieRepository;
-    private final ActorRepository actorRepository;
 
     public MovieContentList addMovieToUserContentList(User user, String listName, @Valid @ImdbId String movieImbdId) {
         Movie movie = movieRepository.findByImdbId(movieImbdId).orElseThrow(MovieNotFoundException::new);
@@ -41,26 +35,6 @@ public class UserService {
         userRepository.save(user);
         log.info("Added movie: `{}` to list: `{}` for user: {}", movie.getTitle(), listName, user.getUsername());
         return movieContentList;
-    }
-
-    public ActorContentList addActorToUserContentList(User user, String listName, @Valid @ImdbId String actorImdbId) {
-        Actor actor = actorRepository.findByImdbId(actorImdbId).orElseThrow();
-        return addActorToUserContentList(user, listName, actor);
-    }
-
-    public ActorContentList addActorToUserContentList(User user, String listName, Actor actor) {
-        ActorContentList actorContentList = user.getActorContentListByName(listName).orElseThrow(NoListWithSuchNameException::new);
-        actorContentList.addActor(actor);
-        userRepository.save(user);
-        log.info("Added actor: `{}` to list: `{}` for user: {}", actor.getName(), listName, user.getUsername());
-        return actorContentList;
-    }
-
-    public ContentList addNewContentListToUser(User user, String listName, Function<String, ContentList> addingFunction) {
-        ContentList contentList = addingFunction.apply(listName);
-        userRepository.save(user);
-        log.info("Added new list `{}` for user: {}", listName, user.getUsername());
-        return contentList;
     }
 
     public MovieContentList addNewMovieContentListToUser(User user, String listName) {
@@ -81,18 +55,6 @@ public class UserService {
         log.info("Movie `{}` removed from list `{}` for user: {}", movieToRemove.getTitle(), listName, user.getUsername());
     }
 
-    public void removeActorFromList(User user, String listName, @Valid @ImdbId String actorImdbIdToRemove) {
-        Actor actorToRemove = actorRepository.findByImdbId(actorImdbIdToRemove).orElseThrow();
-        removeActorFromList(user, listName, actorToRemove);
-    }
-
-    public void removeActorFromList(User user, String listName, Actor actorToRemove) {
-        ActorContentList actorContentList = user.getActorContentListByName(listName).orElseThrow(NoListWithSuchNameException::new);
-        actorContentList.removeActor(actorToRemove);
-        userRepository.save(user);
-        log.info("Actor `{}` removed from list `{}` for user: {}", actorToRemove.getName(), listName, user.getUsername());
-    }
-
     public void removeMovieContentList(User user, String listName) {
         MovieContentList list = user.getMovieContentListByName(listName).orElseThrow(NoListWithSuchNameException::new);
         user.getMovieLists().remove(list);
@@ -100,11 +62,11 @@ public class UserService {
         log.info("Removed list `{}` from user: {}", list, user.getUsername());
     }
 
-    public void removeActorContentList(User user, String listName) {
-        ActorContentList list = user.getActorContentListByName(listName).orElseThrow(NoListWithSuchNameException::new);
-        user.getActorList().remove(list);
+    public void addToWatchMovieList(User user, String listName) {
+        user.getMovieLists().forEach(movieContentList -> movieContentList.setToWatchList(false));
+        user.addNewMovieList(listName).withToWatchList(true);
         userRepository.save(user);
-        log.info("Removed list `{}` from user: {}", list, user.getUsername());
+        log.info("Created new to watch movie list with name `{}`", listName);
     }
 
 
