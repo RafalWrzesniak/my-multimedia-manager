4eaffa695cbb38daea013b0ea135f166fa6d3cd8
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-25 21:23:35 +0100

    Removes unnecessary url from author dto

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/author/AuthorDto.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/author/AuthorDto.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.net.URL;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthorDto {
+
     private String name;
-    private URL url;
+
 }
