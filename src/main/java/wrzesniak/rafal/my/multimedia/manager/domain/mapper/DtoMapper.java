5ac3ef248a097623b64a62e61e48e0a3fc9c4c59
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-27 22:29:45 +0100

    Adds mapping books and authors

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/mapper/DtoMapper.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/mapper/DtoMapper.java
@@ -5,6 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import wrzesniak.rafal.my.multimedia.manager.config.security.LoginCredentials;
 import wrzesniak.rafal.my.multimedia.manager.domain.actor.Actor;
 import wrzesniak.rafal.my.multimedia.manager.domain.actor.ActorDto;
+import wrzesniak.rafal.my.multimedia.manager.domain.author.Author;
+import wrzesniak.rafal.my.multimedia.manager.domain.author.AuthorDto;
+import wrzesniak.rafal.my.multimedia.manager.domain.book.Book;
+import wrzesniak.rafal.my.multimedia.manager.domain.book.BookDto;
+import wrzesniak.rafal.my.multimedia.manager.domain.book.ISBN;
 import wrzesniak.rafal.my.multimedia.manager.domain.movie.Movie;
 import wrzesniak.rafal.my.multimedia.manager.domain.movie.MovieDto;
 import wrzesniak.rafal.my.multimedia.manager.domain.user.User;
@@ -15,6 +20,7 @@ import java.util.List;
 
 import static lombok.AccessLevel.PRIVATE;
 import static wrzesniak.rafal.my.multimedia.manager.domain.user.UserRole.USER;
+import static wrzesniak.rafal.my.multimedia.manager.util.StringFunctions.toURL;
 
 @Slf4j
 @NoArgsConstructor(access = PRIVATE)
@@ -38,7 +44,7 @@ public class DtoMapper {
         log.info("Trying to map dto to movie: {}", movieDto);
         return Movie.builder()
                 .imdbId(movieDto.getId())
-                .title(movieDto.getTitle())
+                .title(firstNotEmpty(movieDto.getOriginalTitle(), movieDto.getTitle()))
                 .filmwebUrl(movieDto.getFilmwebUrl())
                 .polishTitle(movieDto.getWikipedia().titleInLanguage())
                 .releaseDate(movieDto.getReleaseDate())
@@ -72,4 +78,29 @@ public class DtoMapper {
                 .toList();
     }
 
+    private static String firstNotEmpty(String title1, String title2) {
+        return title1 != null && !title1.isEmpty() ? title1 : title2;
+    }
+
+    public static Book mapToBook(BookDto bookDto) {
+        return Book.builder()
+                .title(bookDto.getName())
+                .category(bookDto.getGenre().substring(bookDto.getGenre().lastIndexOf("/") + 1))
+                .description(bookDto.getDescription())
+                .publisher(bookDto.getPublisher())
+                .numberOfPages(bookDto.getNumberOfPages())
+                .isbn(ISBN.of(bookDto.getIsbn()))
+                .lubimyCzytacUrl(toURL(bookDto.getUrl()))
+                .datePublished(bookDto.getDatePublished())
+                .createdOn(LocalDate.now())
+                .build();
+    }
+
+    public static Author mapToAuthor(AuthorDto authorDto) {
+        return Author.builder()
+                .name(authorDto.getName())
+                .writtenBooks(new ArrayList<>())
+                .createdOn(LocalDate.now())
+                .build();
+    }
 }
