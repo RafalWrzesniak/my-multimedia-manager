8474bb5b7c2c6a60fea135ec45742d27e33afda3
Rafał <rafal.wrzesniak94@gmail.com>
2023-01-29 13:59:55 +0100

    Removes unnecessary migration part code

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/MyMultimediaManagerApplication.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/MyMultimediaManagerApplication.java
@@ -1,33 +1,17 @@
 package wrzesniak.rafal.my.multimedia.manager;
 
 import lombok.extern.slf4j.Slf4j;
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.event.EventListener;
-
-import javax.sql.DataSource;
 
 @Slf4j
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class MyMultimediaManagerApplication {
 
-	@Autowired
-	private DataSource dataSource;
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyMultimediaManagerApplication.class, args);
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void initializeDatabaseWithFlyway() {
-		log.info("Initializing database with Flyway");
-		Flyway flyway = Flyway.configure().dataSource(dataSource).load();
-		flyway.migrate();
 	}
 
 }
