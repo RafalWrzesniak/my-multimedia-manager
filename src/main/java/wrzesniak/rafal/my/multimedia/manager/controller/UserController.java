6348bdfe6dfaee207a33ea0ad635b6009ac46e00
Rafał <rafal.wrzesniak94@gmail.com>
2023-02-15 21:46:41 +0100

    Adds endpoint for general list info for user

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/UserController.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/UserController.java
@@ -5,9 +5,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wrzesniak.rafal.my.multimedia.manager.domain.book.user.details.BookListWithUserDetails;
+import wrzesniak.rafal.my.multimedia.manager.domain.content.ActorContentList;
 import wrzesniak.rafal.my.multimedia.manager.domain.error.NoSuchUserException;
+import wrzesniak.rafal.my.multimedia.manager.domain.movie.user.details.MovieListWithUserDetails;
 import wrzesniak.rafal.my.multimedia.manager.domain.user.User;
 import wrzesniak.rafal.my.multimedia.manager.domain.user.UserRepository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("user")
@@ -22,4 +29,14 @@ public class UserController {
         return userRepository.findByUsername(username).orElseThrow(NoSuchUserException::new);
     }
 
+    @GetMapping("/lists")
+    public List<?> getGeneralListsInfo() {
+        return Stream.of(
+                        getCurrentUser().getMovieLists().stream().map(MovieListWithUserDetails::of).toList(),
+                        getCurrentUser().getActorList().stream().map(ActorContentList::toDto).toList(),
+                        getCurrentUser().getBookLists().stream().map(BookListWithUserDetails::of).toList()
+                )
+                .flatMap(Collection::stream)
+                .toList();
+    }
 }
