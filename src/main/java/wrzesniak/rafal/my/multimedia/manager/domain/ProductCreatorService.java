bbd0fe4f76b721a58a5b785c5be605756e6acdd7
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-04 18:57:53 +0100

    Moves ProductCreatorService to proper package

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/ProductCreatorService.java
+++
@@ -1,9 +0,0 @@
-package wrzesniak.rafal.my.multimedia.manager.domain;
-
-import java.net.URL;
-
-public interface ProductCreatorService<PRODUCT> {
-
-    PRODUCT createProductFromUrl(URL url);
-
-}
---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/product/ProductCreatorService.java
@@ -0,0 +1,9 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.product;
+
+import java.net.URL;
+
+public interface ProductCreatorService<PRODUCT_WITH_USER_DETAILS> {
+
+    PRODUCT_WITH_USER_DETAILS createProductFromUrl(URL url);
+
+}
