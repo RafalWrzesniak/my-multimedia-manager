1442395709f070cda7fc4ac4e4c5126f3363f22c
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-04 17:54:11 +0100

    Adds space to available chars in nicknames

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/config/security/LoginCredentials.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/config/security/LoginCredentials.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 public class LoginCredentials {
 
-    public static final String USERNAME_VALIDATION_REGEX = "^[a-zA-Z][\\w.@_]{3,32}$";
+    public static final String USERNAME_VALIDATION_REGEX = "^[a-zA-Z][\\w.@_ ]{3,32}$";
     public static final String USERNAME_VALIDATION_MESSAGE = "Username must be between 4-50 letters, start with letter and contain only letters, digits, dots, at signs and underscore";
     public static final String PASSWORD_VALIDATION_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$";
     public static final String PASSWORD_VALIDATION_MESSAGE = "Password must has minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
