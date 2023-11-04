e3c1ce60ef8eeb3ccdd365dd88aa9813ca485d71
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-04 19:01:26 +0100

    Adds default dynamo client and repository

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/dynamodb/DefaultDynamoRepository.java
@@ -0,0 +1,101 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.dynamodb;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import wrzesniak.rafal.my.multimedia.manager.domain.product.Finishable;
+import wrzesniak.rafal.my.multimedia.manager.domain.product.IdSupport;
+import wrzesniak.rafal.my.multimedia.manager.domain.product.Product;
+import wrzesniak.rafal.my.multimedia.manager.domain.product.Updatable;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.UserService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+import static wrzesniak.rafal.my.multimedia.manager.config.CacheConfiguration.*;
+
+@Slf4j
+@RequiredArgsConstructor
+public class DefaultDynamoRepository<
+        PRODUCT_WITH_USER_DETAILS,
+        PRODUCT_USER_DETAILS extends Finishable<PRODUCT_USER_DETAILS> & IdSupport & Updatable<PRODUCT_USER_DETAILS>,
+        PRODUCT extends Product> {
+
+    private final DynamoDbClientGeneric<PRODUCT> productDynamoClient;
+    private final DynamoDbClientGeneric<PRODUCT_USER_DETAILS> productUserDetailsDynamoClient;
+    private final UserService userService;
+    private final BiFunction<String, String, PRODUCT_USER_DETAILS> createNewUserDetailsFunction;
+    private final BiFunction<PRODUCT, PRODUCT_USER_DETAILS, PRODUCT_WITH_USER_DETAILS> mergeProductWithUserDetails;
+
+    public PRODUCT_WITH_USER_DETAILS saveProduct(PRODUCT product) {
+        productDynamoClient.saveItem(product);
+        createOrUpdateUserDetailsFor(product.getId());
+        return getById(product.getId()).orElseThrow(() -> new IllegalStateException("Cannot find product that should have been saved: " + product));
+    }
+
+    public void createOrUpdateUserDetailsFor(String productId) {
+        PRODUCT_USER_DETAILS userDetails = productUserDetailsDynamoClient.getItemById(owner(), productId)
+                .map(userDetails1 -> userDetails1.withUpdatedOn(LocalDateTime.now()))
+                .orElse(createNewUserDetailsFunction.apply(owner(), productId));
+        productUserDetailsDynamoClient.saveItem(userDetails);
+    }
+
+    public Optional<PRODUCT_WITH_USER_DETAILS> getById(String productId) {
+        Optional<PRODUCT> product = productDynamoClient.getItemById(productId);
+        PRODUCT_USER_DETAILS userDetails = getProductUserDetails(productId);
+        return product.map(prod -> mergeProductWithUserDetails.apply(prod, userDetails));
+    }
+
+    public PRODUCT_WITH_USER_DETAILS mergeProductWithDetails(PRODUCT product, PRODUCT_USER_DETAILS userDetails) {
+        return mergeProductWithUserDetails.apply(product, userDetails);
+    }
+
+    @Cacheable(value = RECENTLY_DONE_CACHE)
+    public List<PRODUCT_WITH_USER_DETAILS> findRecentlyDone(int limit) {
+        return productUserDetailsDynamoClient.findObjectsByPartitionKey(owner()).stream()
+                .filter(userDetails -> userDetails.getFinishedOn() != null)
+                .sorted((m1, m2) -> m2.getFinishedOn().compareTo(m1.getFinishedOn()))
+                .limit(limit)
+                .map(userDetails -> getById(userDetails.getId()))
+                .map(Optional::get)
+                .toList();
+    }
+
+    @Caching(cacheable = {
+            @Cacheable(value = BOOK_USER_DETAILS_CACHE, key = "#productId", condition="#productId.contains('lubimyczytac')"),
+            @Cacheable(value = GAME_USER_DETAILS_CACHE, key="#productId", condition="#productId.contains('gry-online')"),
+            @Cacheable(value = MOVIE_USER_DETAILS_CACHE, key="#productId", condition="#productId.contains('filmweb')")})
+    public PRODUCT_USER_DETAILS getProductUserDetails(String productId) {
+        log.info("Getting product user details for product {}", productId);
+        return productUserDetailsDynamoClient.getItemById(owner(), productId)
+                .orElse(createNewUserDetailsFunction.apply(owner(), productId));
+    }
+
+    @Caching(put = {
+            @CachePut(value = BOOK_USER_DETAILS_CACHE, key = "#productUserDetails.getId()", condition = "#productUserDetails.getId().contains('lubimyczytac')"),
+            @CachePut(value = GAME_USER_DETAILS_CACHE, key = "#productUserDetails.getId()", condition = "#productUserDetails.getId().contains('gry-online')"),
+            @CachePut(value = MOVIE_USER_DETAILS_CACHE, key = "#productUserDetails.getId()", condition = "#productUserDetails.getId().contains('filmweb')")})
+    public PRODUCT_USER_DETAILS updateUserDetails(PRODUCT_USER_DETAILS productUserDetails) {
+        log.info("Updating product user details {}", productUserDetails);
+        productUserDetailsDynamoClient.updateItem(productUserDetails.withUpdatedOn(LocalDateTime.now()));
+        return productUserDetails;
+    }
+
+    @Caching(cacheable = {
+            @Cacheable(value = BOOK_DETAILS_CACHE, key = "#productId", condition="#productId.contains('lubimyczytac')"),
+            @Cacheable(value = GAME_DETAILS_CACHE, key="#productId", condition="#productId.contains('gry-online')"),
+            @Cacheable(value = MOVIE_DETAILS_CACHE, key="#productId", condition="#productId.contains('filmweb')")})
+    public Optional<PRODUCT> getRawProductById(String productId) {
+        log.info("Getting product details for product {}", productId);
+        return productDynamoClient.getItemById(productId);
+    }
+
+    private String owner() {
+        return userService.getCurrentUsername();
+    }
+
+}
---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/dynamodb/DynamoDbClientGeneric.java
@@ -0,0 +1,68 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.dynamodb;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class DynamoDbClientGeneric<TYPE> {
+
+    private final DynamoDbTable<TYPE> table;
+
+    public DynamoDbClientGeneric(DynamoDbEnhancedClient enhancedClient, Class<TYPE> typeClass, String tableName) {
+         table = enhancedClient.table(tableName, TableSchema.fromBean(typeClass));
+    }
+
+    public List<TYPE> getAllObjects() {
+        return table.scan().items().stream().toList();
+    }
+
+    public Optional<TYPE> getItemById(String partitionKey) {
+        return Optional.ofNullable(table.getItem(Key.builder()
+                .partitionValue(partitionKey)
+                .build()));
+    }
+
+    public Optional<TYPE> getItemById(String partitionKey, String sortKey) {
+        return Optional.ofNullable(table.getItem(Key.builder()
+                .partitionValue(partitionKey)
+                .sortValue(sortKey)
+                .build()));
+    }
+
+    public List<TYPE> findObjectsByPartitionKey(String partitionKey) {
+        return table.query(QueryConditional.keyEqualTo(Key.builder()
+                    .partitionValue(partitionKey)
+                    .build()))
+                .items()
+                .stream()
+                .toList();
+    }
+
+    public void saveItem(TYPE item) {
+        log.info("Saving item: {}", item);
+        table.putItem(item);
+    }
+
+    public void updateItem(TYPE item) {
+        log.info("Updating item: {}", item);
+        table.updateItem(item);
+    }
+
+    public void removeItem(String itemId) {
+        log.info("Removing item with id: {}", itemId);
+        getItemById(itemId).ifPresent(table::deleteItem);
+    }
+
+    public void removeItem(String partitionKey, String sortKey) {
+        log.info("Removing item with id: {}: {}", partitionKey, sortKey);
+        getItemById(partitionKey, sortKey).ifPresent(table::deleteItem);
+    }
+
+}
