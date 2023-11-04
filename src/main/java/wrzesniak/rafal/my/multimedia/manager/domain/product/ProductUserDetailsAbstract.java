73a63b3e576649b1da19d028b050246a380e8232
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-04 18:49:07 +0100

    Adds basic products interfaces

---
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/domain/product/ProductUserDetailsAbstract.java
@@ -0,0 +1,5 @@
+package wrzesniak.rafal.my.multimedia.manager.domain.product;
+
+public abstract class ProductUserDetailsAbstract<USER_DETAILS> implements Updatable<USER_DETAILS>, IdSupport, Finishable<USER_DETAILS> {
+
+}
