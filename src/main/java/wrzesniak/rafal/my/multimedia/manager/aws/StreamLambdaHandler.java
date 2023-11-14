3ed810f27ffe93e51ea38745ff776b910b3a8198
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-14 23:05:22 +0100

    Updated sam file

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/aws/StreamLambdaHandler.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/aws/StreamLambdaHandler.java
@@ -6,30 +6,40 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import wrzesniak.rafal.my.multimedia.manager.MyMultimediaManagerApplication;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
 
-    @Slf4j
-    public class StreamLambdaHandler implements RequestStreamHandler {
+public class StreamLambdaHandler implements RequestStreamHandler {
 
-        private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
-        static {
-            try {
-                handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(MyMultimediaManagerApplication.class);
-            } catch (ContainerInitializationException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Could not initialize Spring Boot application", e);
-            }
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(MyMultimediaManagerApplication.class);
+        } catch (ContainerInitializationException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
-
-        @Override
-        public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-            handler.proxyStream(inputStream, outputStream, context);
-        }
-
     }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+
+        AwsProxyResponse response = new AwsProxyResponse();
+        response.getMultiValueHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
+        response.getMultiValueHeaders().put("Access-Control-Allow-Headers", Arrays.asList("Content-Type", "X-Amz-Date", "Authorization"));
+        response.getMultiValueHeaders().put("Access-Control-Allow-Methods", Collections.singletonList("OPTIONS,POST,GET,PUT,DELETE"));
+
+        String jsonResponse = new ObjectMapper().writeValueAsString(response);
+        outputStream.write(jsonResponse.getBytes(StandardCharsets.UTF_8));
+    }
+}
+
