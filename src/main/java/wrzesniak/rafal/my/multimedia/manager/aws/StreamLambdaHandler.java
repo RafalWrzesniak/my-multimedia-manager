8658bb3b50a2b47c534b17f36f7e8a3841d783a0
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-17 15:41:25 +0100

    Comments out prints in lamda

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/aws/StreamLambdaHandler.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/aws/StreamLambdaHandler.java
@@ -6,16 +6,12 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import wrzesniak.rafal.my.multimedia.manager.MyMultimediaManagerApplication;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Collections;
 
 @Slf4j
 public class StreamLambdaHandler implements RequestStreamHandler {
@@ -36,18 +32,18 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         log.info("Before handling request");
-        log.info(new ObjectMapper().writeValueAsString(inputStream));
+//        log.info(new ObjectMapper().writeValueAsString(inputStream));
         handler.proxyStream(inputStream, outputStream, context);
         log.info("After handling request");
-        log.info(new ObjectMapper().writeValueAsString(outputStream));
-
-        AwsProxyResponse response = new AwsProxyResponse();
-        response.getMultiValueHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
-        response.getMultiValueHeaders().put("Access-Control-Allow-Headers", Arrays.asList("Content-Type", "X-Amz-Date", "Authorization"));
-        response.getMultiValueHeaders().put("Access-Control-Allow-Methods", Collections.singletonList("OPTIONS,POST,GET,PUT,DELETE"));
-
-        String jsonResponse = new ObjectMapper().writeValueAsString(response);
-        outputStream.write(jsonResponse.getBytes(StandardCharsets.UTF_8));
+//        log.info(new ObjectMapper().writeValueAsString(outputStream));
+//
+//        AwsProxyResponse response = new AwsProxyResponse();
+//        response.getMultiValueHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
+//        response.getMultiValueHeaders().put("Access-Control-Allow-Headers", Arrays.asList("Content-Type", "X-Amz-Date", "Authorization"));
+//        response.getMultiValueHeaders().put("Access-Control-Allow-Methods", Collections.singletonList("OPTIONS,POST,GET,PUT,DELETE"));
+//
+//        String jsonResponse = new ObjectMapper().writeValueAsString(response);
+//        outputStream.write(jsonResponse.getBytes(StandardCharsets.UTF_8));
     }
 }
 
