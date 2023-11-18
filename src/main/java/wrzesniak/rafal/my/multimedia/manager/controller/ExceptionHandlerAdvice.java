950e07e4c5872f38c14e6e831cc81109d72a83b9
Rafał <rafal.wrzesniak94@gmail.com>
2023-11-18 11:54:23 +0100

    Removes spring security for a moment

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/ExceptionHandlerAdvice.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/ExceptionHandlerAdvice.java
@@ -1,52 +1,38 @@
 package wrzesniak.rafal.my.multimedia.manager.controller;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import wrzesniak.rafal.my.multimedia.manager.domain.error.BasicApplicationException;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-
-@ControllerAdvice
+//@ControllerAdvice
 public class ExceptionHandlerAdvice {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, Object>> handleValidationException(ConstraintViolationException e) {
-        List<String> messages = e.getConstraintViolations().stream()
-                .map(ConstraintViolation::getMessageTemplate)
-                .toList();
-
-        return buildResponseEntity(messages);
-    }
-
-    @ExceptionHandler(BasicApplicationException.class)
-    public ResponseEntity<Map<String, Object>> handleValidationException(BasicApplicationException e) {
-        return buildResponseEntity(List.of(e.getClass().getSimpleName()));
-    }
-
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<Map<String, Object>> handleValidationException(BindException e) {
-        List<String> errors = e.getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
-
-        return buildResponseEntity(errors);
-    }
-
-    private ResponseEntity<Map<String, Object>> buildResponseEntity(List<String> messages) {
-        return ResponseEntity
-                .badRequest()
-                .body(Map.of(
-                        "status", HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                        "code", HttpStatus.BAD_REQUEST.value(),
-                        "messages", messages,
-                        "timestamp", LocalDateTime.now()));
-    }
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    public ResponseEntity<Map<String, Object>> handleValidationException(ConstraintViolationException e) {
+//        List<String> messages = e.getConstraintViolations().stream()
+//                .map(ConstraintViolation::getMessageTemplate)
+//                .toList();
+//
+//        return buildResponseEntity(messages);
+//    }
+//
+//    @ExceptionHandler(BasicApplicationException.class)
+//    public ResponseEntity<Map<String, Object>> handleValidationException(BasicApplicationException e) {
+//        return buildResponseEntity(List.of(e.getClass().getSimpleName()));
+//    }
+//
+//    @ExceptionHandler(BindException.class)
+//    public ResponseEntity<Map<String, Object>> handleValidationException(BindException e) {
+//        List<String> errors = e.getAllErrors().stream()
+//                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                .toList();
+//
+//        return buildResponseEntity(errors);
+//    }
+//
+//    private ResponseEntity<Map<String, Object>> buildResponseEntity(List<String> messages) {
+//        return ResponseEntity
+//                .badRequest()
+//                .body(Map.of(
+//                        "status", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+//                        "code", HttpStatus.BAD_REQUEST.value(),
+//                        "messages", messages,
+//                        "timestamp", LocalDateTime.now()));
+//    }
 }
