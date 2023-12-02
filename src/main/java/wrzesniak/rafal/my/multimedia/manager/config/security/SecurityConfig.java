6d51026d8ca270c48fb1deeaff701eea11fe4fb9
Rafał <rafal.wrzesniak94@gmail.com>
2023-12-02 20:06:53 +0100

    Changes security config

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/config/security/SecurityConfig.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/config/security/SecurityConfig.java
@@ -2,27 +2,25 @@ package wrzesniak.rafal.my.multimedia.manager.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 import wrzesniak.rafal.my.multimedia.manager.domain.user.DynamoUserDetailService;
 
-import javax.servlet.DispatcherType;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -54,33 +52,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests(request -> request
-//                            .antMatchers("/login").permitAll()
-//                            .antMatchers("/prod/login").permitAll()
-//                            .antMatchers("/register").permitAll()
-//                            .antMatchers("/simple").permitAll()
-//                            .antMatchers("/prod/simple").permitAll()
-//                            .antMatchers("/error").permitAll()
-//                            .anyRequest().authenticated()
-//            )
-//            .headers()
-//            .and().exceptionHandling()
-//            .and().httpBasic(withDefaults())
-//            .addFilter(authenticationFilter())
-//            .addFilter(new JwtAuthorizationFilter(authenticationManager(), dynamoUserDetailService, secret))
-//            .sessionManagement().sessionCreationPolicy(STATELESS)
-//            .and().cors().and().csrf().disable();
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/prod/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(authenticationFilter())
-                .addFilter(new JwtAuthorizationFilter(authenticationManager(), dynamoUserDetailService, secret))
-                .httpBasic()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
-
+        http
+            .requiresChannel().anyRequest().requiresSecure()
+            .and().authorizeRequests()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/prod/login").permitAll()
+            .antMatchers("/register").permitAll()
+            .antMatchers("/simple").permitAll()
+            .antMatchers("/prod/simple").permitAll()
+            .antMatchers("/error").permitAll()
+            .anyRequest().authenticated()
+            .and().headers()
+            .and().exceptionHandling()
+            .and().httpBasic(withDefaults())
+            .addFilter(authenticationFilter())
+            .addFilter(new JwtAuthorizationFilter(authenticationManager(), dynamoUserDetailService, secret))
+            .sessionManagement().sessionCreationPolicy(STATELESS)
+            .and().cors().and().csrf().disable();
     }
 
     public JsonObjectAuthenticationFilter authenticationFilter() throws Exception {
@@ -99,26 +87,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedOrigins(List.of("https://master.d5i6lke35o8tb.amplifyapp.com", "http://localhost:3000"));
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("https://master.d5i6lke35o8tb.amplifyapp.com", "http://localhost:3000"));
+//        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTION"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(List.of("X-Requested-With","Origin","Content-Type","Accept","Authorization","x-xsrf-token"));
         configuration.setExposedHeaders(List.of("Authorization", "x-xsrf-token", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-    @Bean
-    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
-        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
-        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
-        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        registration.setUrlPatterns(List.of("*"));
-        return registration;
-    }
+//    @Bean
+//    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+//        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+//        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+//        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+//        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        registration.setUrlPatterns(List.of("*"));
+//        return registration;
+//    }
 
     @Bean
     public CommonsRequestLoggingFilter logFilter() {
