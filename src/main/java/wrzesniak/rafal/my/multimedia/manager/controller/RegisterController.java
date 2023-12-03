202e962d2abd18da4b5def27193fab172f7d0ca4
Rafał <rafal.wrzesniak94@gmail.com>
2023-12-03 16:50:04 +0100

    Revert "Revert "Turns off security""
    This reverts commit 0b1b7dc1

--- src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/RegisterController.java
+++ src/main/java/wrzesniak/rafal/my/multimedia/manager/controller/RegisterController.java
@@ -2,7 +2,6 @@ package wrzesniak.rafal.my.multimedia.manager.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wrzesniak.rafal.my.multimedia.manager.config.security.LoginCredentials;
 import wrzesniak.rafal.my.multimedia.manager.domain.user.RegistrationService;
+import wrzesniak.rafal.my.multimedia.manager.domain.user.UserDynamo;
 
 import javax.validation.Valid;
 
@@ -24,7 +24,7 @@ public class RegisterController {
     private final RegistrationService registrationService;
 
     @PostMapping
-    public UserDetails registerNewUser(@Valid LoginCredentials credentials) {
+    public UserDynamo registerNewUser(@Valid LoginCredentials credentials) {
         log.info("Trying to register new user: {}", credentials.getUsername());
         return registrationService.registerNewUserAccount(credentials);
     }
